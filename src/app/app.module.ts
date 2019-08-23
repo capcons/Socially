b6a312d647c4affc330086d98d9d7c73691f98a0
firoz2580 <firoz2580@gmail.com>
2019-08-23 14:55:09 +0530

    Updated app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -54,26 +54,7 @@ import { MaterialModule } from './Modules/Material.module';
     SharedModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularFireModule.initializeApp(
-      {
-        apiKey: "AIzaSyC0m-8NJoZUYGl6E3LCJ-5y2oBu0byGMDo",
-        authDomain: "socially-62c2c.firebaseapp.com",
-        databaseURL: "https://socially-62c2c.firebaseio.com",
-        projectId: "socially",
-        storageBucket: "socially.appspot.com",
-        messagingSenderId: "295137985071",
-        appId: "1:295137985071:web:30d3b67eb758db42"
-      }
-      // OLD - Kachings project
-      //   {
-      //   apiKey: "AIzaSyDuWvSQv22P_riHV-nwkWXN_pg1o3VRIGg",
-      //   authDomain: "kachings-83e4e.firebaseapp.com",
-      //   databaseURL: "https://kachings-83e4e.firebaseio.com",
-      //   projectId: "kachings-83e4e",
-      //   storageBucket: "kachings-83e4e.appspot.com",
-      //   messagingSenderId: "355576704221"
-      // }
-    ),
+    
     AngularFireAuthModule,
     AngularFireAuthGuardModule,
     AngularFirestoreModule, //.enablePersistence(),
