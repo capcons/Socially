736c67e8553483850937b2cd5cba5c4e4f733064
firoz2580 <firoz2580@gmail.com>
2019-08-25 19:52:27 +0530

    Implement search feature

--- src/app/Modules/FeatureRoutings/search-routing.module.ts
+++ src/app/Modules/FeatureRoutings/search-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchComponent } from 'src/app/components/search/search.component';
 import { AuthGuard } from 'src/app/guards/auth.guard';
