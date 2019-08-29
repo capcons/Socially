8829e80437dcff163941bca05b795b0ac7faf24f
firoz2580 <firoz2580@gmail.com>
2019-08-29 19:38:54 +0530

    Improved router navigations

--- src/app/components/customElements/mini-navbar/mini-navbar.component.ts
+++ src/app/components/customElements/mini-navbar/mini-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MyAuthService } from 'src/app/Services/my-auth.service';
 
 @Component({
   selector: 'element-mini-navbar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiniNavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public MyAuth: MyAuthService) { }
 
   ngOnInit() {
   }
